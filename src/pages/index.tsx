c574014821390b3fb5322272308abba9b1d7f9f6
Ariane <ariianeboorges@gmail.com>
2021-02-23 21:26:47 -0300

    mudando projeto para next

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import React from "react";
+import { ExperienceBar } from "../components/ExperienceBar";
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+       
+      </Head>
+
+      <ExperienceBar />
+    </div>
+  );
+}
